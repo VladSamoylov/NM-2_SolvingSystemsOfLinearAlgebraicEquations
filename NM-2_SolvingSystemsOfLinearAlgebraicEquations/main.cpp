0f9c14238a0746df40f94f3e653c7c354e1ca4b1
Влад <@->
2024-11-28 15:49:05 +0200

    Добавьте файлы проекта.

---
+++ NM-2_SolvingSystemsOfLinearAlgebraicEquations/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void GetDet(vector<vector<double>>& matrix) {
+
+}
+
+
+
+int main() {
+
+	vector<vector<double>> matrix = {
+		{ 1, -1, -1},
+		{-2, 1, 2},
+		{3, 3, -1}
+	};
+	vector<double> B = { 0, -1, 8 };
+
+
+	cout << "What's the hell?" << endl;
+		
+
+	return 0;
+}

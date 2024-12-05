85cd20f4fe4a5ff695dc57f1d363d091e645d9db
Vlad Samoylov <vladsamoylovvlad@gmail.com>
2024-12-05 03:45:04 +0200

    create all task

--- NM-2_SolvingSystemsOfLinearAlgebraicEquations/main.cpp
+++ NM-2_SolvingSystemsOfLinearAlgebraicEquations/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <iomanip>
 
 #define ANSI_COLOR_BLUE "\033[34m"
 #define ANSI_COLOR_RESET "\033[0m"
@@ -10,6 +11,26 @@
 
 using namespace std;
 
+/**
+ * @brief Функція, яка відображує вміст матриці коефіцієнтів Х
+ * @param matrix Матриця коефіцієнтів Х
+ */
+void MatrixShow(const vector<vector<double>>& matrix) {
+
+	for (int i = 0; i < matrix.size(); i++) {
+		for (int j = 0; j < matrix[0].size(); j++) {
+			cout << setw(12) << left << fixed << setprecision(4) << matrix[i][j] << " ";
+		}cout << endl;
+	}cout.unsetf(ios::fixed);
+}
+
+/**
+ * @brief Функція знаходження мінору матриці
+ * @param matrix Матриця коефіцієнтів Х
+ * @param row Рядок для видалення
+ * @param column Стовпець для видалення
+ * @return Нової матриці після видалення
+ */
 vector<vector<double>> GetMinor(const vector<vector<double>>& matrix, const int& row, const int& column) {
 
 	vector<vector<double>> minor;
@@ -25,6 +46,11 @@ vector<vector<double>> GetMinor(const vector<vector<double>>& matrix, const int&
 	return minor;
 }
 
+/**
+ * @brief Функція знаходження визначника матриці
+ * @param matrix Матриця
+ * @return Визначник матриці
+ */
 double GetDeterminate(const vector<vector<double>>& matrix) {
 
 	if (matrix.size() == 1) return matrix[0][0];
@@ -44,6 +70,13 @@ double GetDeterminate(const vector<vector<double>>& matrix) {
 	return det;
 }
 
+/**
+ * @brief Функція заміни стовпця на вектор вільних членів
+ * @param matrix Матриця коефіцієнтів Х
+ * @param B Вектор вільних членів
+ * @param column Стовпець заміни
+ * @return Матриця з заміним стовпцем на вільні члени
+ */
 vector<vector<double>> ReplaceMatrixColumn(const vector<vector<double>>& matrix, const vector<double>& B, const int& column) {
 
 	vector<vector<double>> newMatrix(matrix.size(), vector<double>(matrix[0].size(), 0));
@@ -57,8 +90,16 @@ vector<vector<double>> ReplaceMatrixColumn(const vector<vector<double>>& matrix,
 	return newMatrix;
 }
 
+/**
+ * @brief Функція, яка реалізує метод Крамера для вирішення СЛАР
+ * @param matrix Матриця А
+ * @param B Вектор вільних членів
+ */
 void CramerMethod(const vector<vector<double>>& matrix, const vector<double>& B) {
 
+	cout << "-----Welcome to Cramer Method-----" << endl;
+	cout << "A : " << endl;
+	MatrixShow(matrix);
 	if (matrix.empty() || matrix[0].size() != matrix.size() || B.size() != matrix.size()) {
 		throw "Error: <Invalid matrix size>";
 	}
@@ -67,31 +108,28 @@ void CramerMethod(const vector<vector<double>>& matrix, const vector<double>& B)
 	vector<double> x;
 	det = GetDeterminate(matrix);
 	if (abs(det) < 1e-9) throw "Error: <System has no unique solution (det(A) = 0)";
+	else cout << "det A = " << ANSI_COLOR_RED << det << ANSI_COLOR_RESET << endl;
 	for (int i = 0; i < matrix.size(); i++) {
 		detX.push_back(GetDeterminate(ReplaceMatrixColumn(matrix, B, i)));
 	}
 	cout << "detXn : ";
 	for (auto i : detX) {
-		cout << i << " ";
+		cout << ANSI_COLOR_RED << i << " " << ANSI_COLOR_RESET;
 	}cout << endl;
 	for (int i = 0; i < matrix.size(); i++) {
 		x.push_back(detX[i] / det);
 	}
 	cout << "Xn : ";
 	for (auto i : x) {
-		cout << i << " ";
-	}cout << endl;
+		cout << fixed << setprecision(3) << ANSI_COLOR_GREEN << i << " " << ANSI_COLOR_RESET;
+	}cout << endl; cout.unsetf(ios::fixed);
 }
 
-void MatrixShow(const vector<vector<double>>& matrix) {
-
-	for (int i = 0; i < matrix.size(); i++) {
-		for (int j = 0; j < matrix[0].size(); j++) {
-			cout << matrix[i][j] << " ";
-		}cout << endl;
-	}
-}
-
+/**
+ * @brief Функція, яка знаходить обернену матрицю до матриці А
+ * @param matrix Матриця А
+ * @return Обернена матриця до матриці А
+ */
 vector<vector<double>> GetInvertedMatrix(const vector<vector<double>>& matrix) {
 
 	double sign;
@@ -102,37 +140,44 @@ vector<vector<double>> GetInvertedMatrix(const vector<vector<double>>& matrix) {
 			else sign = -1;
 			matrixComplement[i][j] = sign * (GetDeterminate(GetMinor(matrix, i, j)));		}
 	}
-	cout << "Matrix : \n";
-	MatrixShow(matrixComplement);
 	for (int i = 0; i < matrixComplement.size(); i++) {
 		for (int j = i + 1; j < matrixComplement[0].size(); j++) {
 			swap(matrixComplement[i][j], matrixComplement[j][i]);
 		}
 	}
-	cout << "Matrix : \n";
-	MatrixShow(matrixComplement);
 	double det = GetDeterminate(matrix);
 	for (int i = 0; i < matrixComplement.size(); i++) {
 		for (int j = 0; j < matrixComplement[0].size(); j++) {
 			matrixComplement[i][j] = matrixComplement[i][j] / det;
 		}
 	}
-	cout << "Matrix : \n";
-	MatrixShow(matrixComplement);
 
 	return matrixComplement;
 }
 
+/**
+ * @brief Функція, яка реалізує матричний спосіб вирішення СЛАР
+ * @param matrix Матриця А
+ * @param B Вектор вільних членів
+ */
 void MatrixMethod(const vector<vector<double>>& matrix, const vector<double>& B) {
 
+	cout << "-----Welcome to Matrix Method-----" << endl;
+	cout << "A : " << endl;
+	MatrixShow(matrix);
 	if (matrix.empty() || matrix[0].size() != matrix.size() || B.size() != matrix.size()) {
 		throw "Error: <Invalid matrix size>";
 	}
+	else cout << "I. matrix is square" << endl;
 	vector<vector<double>> tmpMatrix;
 	vector<double> Xn(matrix.size(), 0);
 	double det = GetDeterminate(matrix);
 	if (abs(det) < 1e-9) throw "Error: <System has no unique solution (det(A) = 0)>";
+	else cout << "II. det A = " << ANSI_COLOR_GREEN << det << ANSI_COLOR_RESET << endl;
 	tmpMatrix = GetInvertedMatrix(matrix);
+	cout << "III. A^-1 : " << endl;
+	MatrixShow(tmpMatrix);
+
 	for (int i = 0; i < tmpMatrix.size(); i++) {
 		int bIndex = 0;
 		for (int j = 0; j < tmpMatrix[0].size(); j++) {
@@ -140,12 +185,16 @@ void MatrixMethod(const vector<vector<double>>& matrix, const vector<double>& B)
 			bIndex++;
 		}
 	}
-	cout << "Xn : ";
+	cout << "IV. Xn : ";
 	for (auto i : Xn) {
-		cout << i << " ";
-	}cout << endl;
+		cout << fixed << setprecision(3) << ANSI_COLOR_GREEN << i << " " << ANSI_COLOR_RESET;
+	}cout << endl; cout.unsetf(ios::fixed);
 }
 
+/**
+ * @brief Функція, яка реалізує прямий хід Гауса
+ * @param matrix Матриця
+ */
 void GaussForward(vector<vector<double>>& matrix) {
 
 	for (int i = 0; i < matrix.size(); i++) {
@@ -170,6 +219,11 @@ void GaussForward(vector<vector<double>>& matrix) {
 	}	
 }
 
+/**
+ * @brief Функція,яка реалізує зворотній хід Гауса
+ * @param matrix Матриця
+ * @return Рішення Хn
+ */
 vector<double> GaussReverse(const vector<vector<double>>& matrix) {
 
 	vector<double> Xn(matrix.size(), 0);
@@ -182,28 +236,36 @@ vector<double> GaussReverse(const vector<vector<double>>& matrix) {
 	return Xn;
 }
 
+/**
+ * @brief Функція, яка реалізує метод Гауса для вирішення СЛАР
+ * @param matrix Матриця коефіцієнтів Х
+ * @param B Вектор вільних членів
+ */
 void GaussMethod(const vector<vector<double>>& matrix, const vector<double>& B) {
 
+	cout << "-----Welcome to Gauss Method-----" << endl;
 	if (matrix.empty() || matrix[0].size() != matrix.size() || B.size() != matrix.size()) {
 		throw "Error: <Invalid matrix size>";
 	}
 	vector<vector<double>> matrixExpanded(matrix.size(), vector<double>(matrix[0].size() + 1, 0));
 	vector<double> Xn;
 	int index = 0;
-	cout << matrix.size() << endl;
 	for (int i = 0; i < matrixExpanded.size(); i++) {
 		for (int j = 0; j < matrixExpanded[0].size(); j++) {
 			if (j == matrixExpanded[0].size() - 1) { matrixExpanded[i][j] = B[index]; index++; }
 			else matrixExpanded[i][j] = matrix[i][j];
 		}
 	}
+	cout << "A : " << endl;
+	MatrixShow(matrixExpanded);
+	cout << "after Forward Gauss : " << endl;
 	GaussForward(matrixExpanded);
 	MatrixShow(matrixExpanded);
 	Xn = GaussReverse(matrixExpanded);
 	cout << "Xn : ";
 	for (auto i : Xn) {
-		cout << i << " ";
-	}cout << endl;
+		cout << fixed << setprecision(3) << ANSI_COLOR_GREEN << i << " " << ANSI_COLOR_RESET;
+	}cout << endl; cout.unsetf(ios::fixed);
 }
 
 int main() {
@@ -221,22 +283,33 @@ int main() {
 		{-0.11, -0.84, 0.35, 0.06},
 		{-0.08, 0.15, -0.5, -0.12}
 	};
-	vector<double> B = { 2.15, 0.44, -0.83, 1.16 };
+	vector<double> B_test = { 2.15, 0.44, -0.83, 1.16 };
+
+	vector<vector<double>> matrixTask = {
+		{76, 21, 6, -34},
+		{12, -114, 8, 9},
+		{16, 24, -100, -35},
+		{23, -8, 5, -75}
+	};
+	vector<double> BTask = { -142, 83, -121, 85 };
 
 	try {
 		//MatrixMethod(matrixTest, B);
+		MatrixMethod(matrixTask, BTask);
 	}
 	catch (char* err) {
 		cerr << err << endl;
 	}
 	try {
 		//CramerMethod(matrixTest, B);
+		CramerMethod(matrixTask, BTask);
 	}
 	catch(char* err) {
 		cerr << err << endl;
 	}	
 	try {
-		GaussMethod(matrixTest, B);
+		//GaussMethod(matrixTest, B);
+		GaussMethod(matrixTask, BTask);
 	}
 	catch (char* err) {
 		cerr << err << endl;
